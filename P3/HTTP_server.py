f8a7bae3ba4194595b4dc77c73e608a30563a919
AmirhoseinYari <amirhoseinyari@icloud.com>
2022-06-10 11:45:01 +0430

    PUT is implemented

--- P3/HTTP_server.py
+++ P3/HTTP_server.py
@@ -5,6 +5,8 @@ port = 8000
 
 class MyHttp(http.server.BaseHTTPRequestHandler):
     status = "OK" #changes with posting
+
+    # curl localhost:8000/api/v1/status
     def do_GET(self):
         if self.path == "/api/v1/status":
             if MyHttp.status == "OK":
@@ -12,7 +14,7 @@ class MyHttp(http.server.BaseHTTPRequestHandler):
             else:
                 self.send_response(201)
             
-            self.send_header('Content-type','text/json')
+            self.send_header('Content-Type','application/json')
             self.end_headers()
             massage = json.dumps({"status":MyHttp.status})
 
@@ -20,9 +22,27 @@ class MyHttp(http.server.BaseHTTPRequestHandler):
             self.wfile.write(bytes(massage, 'utf-8'))
         return
     
+    # curl -X POST localhost:8000/api/v1/status -H "Content-Type: application/json" -d '{"status": "not OK"}'
     def do_POST(self):
         if self.path == "/api/v1/status":
-            print("post path ok")
+            #print("post path ok")
+            try:
+                datalen = int(self.headers['Content-Length'])
+                data = json.loads(self.rfile.read(datalen).decode("utf-8"))
+            except:
+                data = {"status":MyHttp.status} # last status
+            
+            #print(data,data['status'],type(data))
+            if data['status'] != MyHttp.status:
+                self.send_response(201)
+                self.send_header('Content-Type','application/json')
+                self.end_headers()
+                MyHttp.status = data['status']
+                massage = json.dumps({"status":MyHttp.status})
+                self.wfile.write(bytes(massage, 'utf-8'))
+            else:
+                self.send_response(200)
+                print('noting changed!')
         return
     
 
